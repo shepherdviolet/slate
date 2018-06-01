76bdba77f7c9b3ac67b1778ad4d778b4c2731bb9
shepherdviolet <shepherdviolet@163.com>
2018-06-01 20:23:18 +0800

    避免低版本JDK报AbstractMethodError

--- src/main/java/sviolet/slate/common/utilx/interfaceinst/InterfaceInstantiationFactoryBean4.java
+++ src/main/java/sviolet/slate/common/utilx/interfaceinst/InterfaceInstantiationFactoryBean4.java
@@ -48,4 +48,9 @@ public class InterfaceInstantiationFactoryBean4<T> implements FactoryBean<T> {
         return clazz;
     }
 
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
 }
