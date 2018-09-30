a3bc2d49def469214213caf5a3b8afdcc2145cd9
shepherdviolet <shepherdviolet@163.com>
2018-09-30 20:14:57 +0800

    增加spring配置类

---
+++ slate-common/src/main/java/sviolet/slate/common/utilx/txtimer/def/DefaultTxTimerSpringConfig.java
@@ -0,0 +1,36 @@
+package sviolet.slate.common.utilx.txtimer.def;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import sviolet.thistle.util.judge.CheckUtils;
+
+/**
+ * 用于Spring容器中修改配置, 将该类注册为Bean即可(仅支持部分配置)
+ *
+ * @author S.Violet
+ */
+@Component
+public class DefaultTxTimerSpringConfig {
+
+    @Value("${slate.txtimer.threshold.avg:}")
+    private void setThresholdAvg(String avg){
+        if (!CheckUtils.isEmptyOrBlank(avg)){
+            DefaultTxTimerConfig.setThresholdAvg(avg);
+        }
+    }
+
+    @Value("${slate.txtimer.threshold.max:}")
+    private void setThresholdMax(String max){
+        if (!CheckUtils.isEmptyOrBlank(max)){
+            DefaultTxTimerConfig.setThresholdMax(max);
+        }
+    }
+
+    @Value("${slate.txtimer.threshold.min:}")
+    private void setThresholdMin(String min){
+        if (!CheckUtils.isEmptyOrBlank(min)){
+            DefaultTxTimerConfig.setThresholdMin(min);
+        }
+    }
+
+}
