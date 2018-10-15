e1a7cdb6e24606067166a9eb9100995696a699bb
shepherdviolet <shepherdviolet@163.com>
2018-10-15 20:27:30 +0800

    修改文档

--- slate-common/src/main/java/sviolet/slate/common/x/conversion/beanutil/SlateBeanUtils.java
+++ slate-common/src/main/java/sviolet/slate/common/x/conversion/beanutil/SlateBeanUtils.java
@@ -2,6 +2,7 @@ package sviolet.slate.common.x.conversion.beanutil;
 
 import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.cglib.beans.BeanMap;
+import org.springframework.objenesis.ObjenesisException;
 import org.springframework.objenesis.SpringObjenesis;
 import sviolet.thistle.model.concurrent.lock.UnsafeSpinLock;
 import sviolet.thistle.x.common.thistlespi.ThistleSpi;
@@ -11,7 +12,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * JavaBean转换工具
+ * Bean转换工具
  *
  * @author S.Violet
  */
@@ -27,12 +28,12 @@ public class SlateBeanUtils {
     private static final Map<String, BeanizationFactory> BEANIZATION_FACTORYS = new ConcurrentHashMap<>(256);
 
     /**
-     * <p>JavaBean参数拷贝</p>
+     * <p>Bean参数拷贝, Bean -> Bean</p>
      * <p>参数类型不匹配时一般不会抛出异常, 会跳过不匹配的参数(参数留空)</p>
-     * <p>内置类型转换器, 可使用ThistleSpi扩展</p>
-     * @param from 从这个JavaBean复制
-     * @param to 复制到这个JavaBean
-     * @throws MappingRuntimeException 拷贝出错(异常概率:低)
+     * <p>内置类型转换器, 当类型不匹配时会尝试转换, 可使用ThistleSpi扩展</p>
+     * @param from 从这个Bean复制
+     * @param to 复制到这个Bean
+     * @throws MappingRuntimeException 异常概率:低, 触发原因: 拷贝器创建失败 / 拷贝过程出错
      */
     public static void copy(Object from, Object to) {
         if (from == null || to == null) {
@@ -55,12 +56,13 @@ public class SlateBeanUtils {
     }
 
     /**
-     * <p>JavaBean参数拷贝, 目的JavaBean自动实例化</p>
+     * <p>Bean参数拷贝, Bean -> Bean</p>
      * <p>参数类型不匹配时一般不会抛出异常, 会跳过不匹配的参数(参数留空)</p>
-     * <p>内置类型转换器, 可使用ThistleSpi扩展</p>
-     * @param from 从这个JavaBean复制
-     * @param toType 目的JavaBean类型
-     * @throws MappingRuntimeException 拷贝出错(异常概率:低)
+     * <p>内置类型转换器, 当类型不匹配时会尝试转换, 可使用ThistleSpi扩展</p>
+     * @param from 从这个Bean复制
+     * @param toType 目的Bean类型
+     * @throws MappingRuntimeException 异常概率:低, 触发原因: 拷贝器创建失败 / 拷贝过程出错
+     * @throws ObjenesisException 异常概率:低, 触发原因: 目标Bean实例化失败
      */
     public static <T> T copy(Object from, Class<T> toType) {
         if (toType == null) {
@@ -76,12 +78,12 @@ public class SlateBeanUtils {
     }
 
     /**
-     * <p>JavaBean转Map</p>
+     * <p>Bean转Map</p>
      * <p>一般不会抛出异常</p>
-     * <p>无类型转换器, 因为Bean转Map不存在类型不匹配</p>
-     * @param fromBean 从这个JavaBean复制
+     * <p>无内置类型转换器, 因为Bean转Map不存在类型不匹配的情况</p>
+     * @param fromBean 从这个Bean复制
      * @param toMap 复制到这个Map
-     * @throws MappingRuntimeException 转换出错(异常概率:低)
+     * @throws MappingRuntimeException 异常概率:低, 触发原因: 映射器创建失败
      */
     public static void toMap(Object fromBean, Map<String, Object> toMap) {
         if (fromBean == null || toMap == null) {
@@ -98,11 +100,11 @@ public class SlateBeanUtils {
     }
 
     /**
-     * <p>JavaBean转Map, 目的Map自动创建</p>
+     * <p>Bean转Map</p>
      * <p>一般不会抛出异常</p>
-     * <p>无类型转换器, 因为Bean转Map不存在类型不匹配</p>
-     * @param fromBean 从这个JavaBean复制
-     * @throws MappingRuntimeException 转换出错(异常概率:低)
+     * <p>无内置类型转换器, 因为Bean转Map不存在类型不匹配的情况</p>
+     * @param fromBean 从这个Bean复制
+     * @throws MappingRuntimeException 异常概率:低, 触发原因: 映射器创建失败
      */
     public static Map<String, Object> toMap(Object fromBean) {
         Map<String, Object> map = new HashMap<>();
@@ -111,14 +113,14 @@ public class SlateBeanUtils {
     }
 
     /**
-     * <p>Map转JavaBean</p>
-     * <p>容易因为Map中字段类型与Bean参数类型不匹配抛出异常, 若设置throwExceptionIfFails为false, 则不会抛出异常(参数留空)</p>
-     * <p>内置类型转换器, 可使用ThistleSpi扩展</p>
+     * <p>Map转Bean</p>
+     * <p>当Map中字段类型与Bean参数类型不匹配时会抛出异常(若设置throwExceptionIfFails为false, 则不会抛出异常, 失败的参数留空)</p>
+     * <p>内置类型转换器, 当类型不匹配时会尝试转换, 可使用ThistleSpi扩展</p>
      * @param fromMap 从这个Map取值
-     * @param toBean 复制到这个JavaBean
-     * @param convert true:尝试转换参数类型使之符合要求, false:不转换参数类型, 若类型不符合则抛出异常或留空
-     * @param throwExceptionIfFails true:如果参数的类型不匹配或转换失败, 则抛出异常, false:如果参数的类型不匹配, 参数留空
-     * @throws MappingRuntimeException 转换出错(异常概率:高), Map中字段类型与Bean参数类型不匹配时会抛出异常(throwExceptionIfFails为true时), 给目的Bean赋值时出错会抛出异常(throwExceptionIfFails无论是true还是false, 均会抛出异常)
+     * @param toBean 复制到这个Bean
+     * @param convert true: 尝试转换参数类型使之符合要求, false: 不转换参数类型
+     * @param throwExceptionIfFails true: 如果参数的类型不匹配或转换失败, 则抛出异常, false: 如果参数的类型不匹配或转换失败, 不会抛出异常, 失败的参数留空
+     * @throws MappingRuntimeException 异常概率:高, 触发原因: Map中字段类型与Bean参数类型不匹配(当throwExceptionIfFails=true) / 给目的Bean赋值时出错(当throwExceptionIfFails=true) / Bean映射器创建失败(无论throwExceptionIfFails为何值, 均抛异常)
      */
     public static void fromMap(Map<String, Object> fromMap, Object toBean, boolean convert, boolean throwExceptionIfFails) {
         if (fromMap == null || toBean == null) {
@@ -145,15 +147,15 @@ public class SlateBeanUtils {
     }
 
     /**
-     * <p>Map转JavaBean</p>
-     * <p>容易因为Map中字段类型与Bean参数类型不匹配抛出异常, 若设置throwExceptionIfFails为false, 则不会抛出异常(参数留空)</p>
-     * <p>内置类型转换器, 可使用ThistleSpi扩展</p>
+     * <p>Map转Bean</p>
+     * <p>当Map中字段类型与Bean参数类型不匹配时会抛出异常(若设置throwExceptionIfFails为false, 则不会抛出异常, 失败的参数留空)</p>
+     * <p>内置类型转换器, 当类型不匹配时会尝试转换, 可使用ThistleSpi扩展</p>
      * @param fromMap 从这个Map取值
-     * @param toType 目的JavaBean类型
-     * @param convert true:尝试转换参数类型使之符合要求, false:不转换参数类型, 若类型不符合则抛出异常或留空
-     * @param throwExceptionIfFails true:如果参数的类型不匹配或转换失败, 则抛出异常, false:如果参数的类型不匹配, 参数留空
-     * @throws MappingRuntimeException 转换出错(异常概率:高), Map中字段类型与Bean参数类型不匹配时会抛出异常(throwExceptionIfFails为true时), 给目的Bean赋值时出错会抛出异常(throwExceptionIfFails无论是true还是false, 均会抛出异常)
-
+     * @param toType 目的Bean类型
+     * @param convert true: 尝试转换参数类型使之符合要求, false: 不转换参数类型
+     * @param throwExceptionIfFails true: 如果参数的类型不匹配或转换失败, 则抛出异常, false: 如果参数的类型不匹配或转换失败, 不会抛出异常, 失败的参数留空
+     * @throws MappingRuntimeException 异常概率:高, 触发原因: Map中字段类型与Bean参数类型不匹配(当throwExceptionIfFails=true) / 给目的Bean赋值时出错(当throwExceptionIfFails=true) / Bean映射器创建失败(无论throwExceptionIfFails为何值, 均抛异常)
+     * @throws ObjenesisException 异常概率:低, 触发原因: 目标Bean实例化失败
      */
     public static <T> T fromMap(Map<String, Object> fromMap, Class<T> toType, boolean convert, boolean throwExceptionIfFails) {
         if (toType == null) {
@@ -168,15 +170,15 @@ public class SlateBeanUtils {
     }
 
     /**
-     * 用于Map转换为Bean前的预处理. 依据指定的JavaBean类型(templateType), 检查Map的参数类型是否符合要求, 若不符合要求,
-     * 则尝试进行类型转换使之符合要求, 若还是不符合要求, 则根据throwExceptionIfFails参数处理(true:抛出异常, false:从Map中剔除不匹配的参数).
+     * 用于Map转换为Bean前的预处理. 依据指定的Bean类型(templateType), 检查Map的参数类型是否符合要求, 若不符合要求,
+     * 则尝试进行类型转换, 若还是不符合, 则根据throwExceptionIfFails参数处理(true:抛出异常, false:从Map中剔除不匹配的参数).
      * (SlateBeanUtils.fromMap方法内部已调用该方法, 在使用fromMap时无需手动调用该方法)
      * @param map 需要进行参数矫正的Map
-     * @param templateType JavaBean类型
-     * @param convert true:转换参数类型使之符合要求, false:不转换参数类型, 不符合则抛出异常或从Map中剔除不匹配的参数
-     * @param throwExceptionIfFails true:如果参数的类型不匹配或转换失败, 则抛出异常, false:如果参数的类型不匹配, 则从Map中剔除该参数
-     * @return 返回矫正后的Map(仅保留需要的参数, 且类型已经过转换, 不匹配的参数可能会被剔除)
-     * @throws MappingRuntimeException 矫正出错(异常概率:高), Map中字段类型与Bean参数类型不匹配时抛出异常(throwExceptionIfFails为true时), 特殊情况也会抛出异常(概率较低, 可能是类型转换器内部实现漏洞, 无论throwExceptionIfFails是true还是false)
+     * @param templateType Bean类型
+     * @param convert true:转换参数类型使之符合要求, false:不转换参数类型
+     * @param throwExceptionIfFails true:如果参数的类型不匹配或转换失败, 则抛出异常, false:如果参数的类型不匹配或转换失败, 则从Map中剔除该参数
+     * @return 返回矫正后的Map(仅保留需要的参数, 且类型已经过转换, 不匹配的参数会被剔除)
+     * @throws MappingRuntimeException 异常概率:高, 触发原因: Map中字段类型与Bean参数类型不匹配(当throwExceptionIfFails=true) / 特殊情况(概率极低, 例如BeanizationFactory有内部漏洞等, 无论throwExceptionIfFails是何值, 均抛出异常)
      */
     public static Map<String, Object> mapBeanization(Map<String, Object> map, Class<?> templateType, boolean convert, boolean throwExceptionIfFails){
         if (map == null || map.size() <= 0 || templateType == null) {
