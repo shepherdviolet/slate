bee031f8bc0780595c66969ec0452f8ee68f6573
shepherdviolet <shepherdviolet@163.com>
2018-10-15 20:17:28 +0800

    添加测试

--- slate-common/src/test/java/sviolet/slate/common/x/conversion/beanutil/SlateBeanUtilsCopyTest.java
+++ slate-common/src/test/java/sviolet/slate/common/x/conversion/beanutil/SlateBeanUtilsCopyTest.java
@@ -1,14 +1,20 @@
 package sviolet.slate.common.x.conversion.beanutil;
 
 import org.junit.Test;
+import sviolet.thistle.util.common.EnvironmentUtils;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SlateBeanUtilsCopyTest {
 
     @Test
-    public void test(){
+    public void test() throws InterruptedException {
+        System.out.println(EnvironmentUtils.PID);
         System.setProperty("slate.beanutils.log", "true");
         System.setProperty("thistle.spi.loglv", "error");
         System.out.println(copyOnce());
@@ -16,48 +22,122 @@ public class SlateBeanUtilsCopyTest {
 
     private static To copyOnce() {
         From from = new From();
-        from.name = "lalala";
-        from.num = new BigDecimal("321");
+        from.p1 = "abc";
+        from.p2 = 21;
+        from.p3 = new BigDecimal("5656.222");
+        from.p4 = new ArrayList<String>(3);
+        from.p4.add("wo1");
+        from.p4.add("wo2");
+        from.p4.add("wo3");
+        from.p5 = new HashMap<String, Object>(2);
+        from.p5.put("m1", "v1");
+        from.p5.put("m2", "v2");
         return SlateBeanUtils.copy(from, To.class);
     }
 
     public static class From {
-        private String name;
-        private BigDecimal num;
-        public String getName() {
-            return name;
+        private String p1;
+        private int p2;
+        private BigDecimal p3;
+        private List p4;
+        private Map p5;
+
+        public String getP1() {
+            return p1;
         }
-        public void setName(String name) {
-            this.name = name;
+
+        public void setP1(String p1) {
+            this.p1 = p1;
         }
-        public BigDecimal getNum() {
-            return num;
+
+        public int getP2() {
+            return p2;
         }
-        public void setNum(BigDecimal num) {
-            this.num = num;
+
+        public void setP2(int p2) {
+            this.p2 = p2;
+        }
+
+        public BigDecimal getP3() {
+            return p3;
+        }
+
+        public void setP3(BigDecimal p3) {
+            this.p3 = p3;
+        }
+
+        public List<String> getP4() {
+            return p4;
+        }
+
+        public void setP4(List<String> p4) {
+            this.p4 = p4;
+        }
+
+        public Map<String, Object> getP5() {
+            return p5;
+        }
+
+        public void setP5(Map<String, Object> p5) {
+            this.p5 = p5;
         }
     }
 
     public static class To {
-        private String name;
-        private long num;
-        public String getName() {
-            return name;
+        private String p1;
+        private int p2;
+        private BigDecimal p3;
+        private List p4;
+        private Map p5;
+
+        public String getP1() {
+            return p1;
         }
-        public void setName(String name) {
-            this.name = name;
+
+        public void setP1(String p1) {
+            this.p1 = p1;
         }
-        public long getNum() {
-            return num;
+
+        public int getP2() {
+            return p2;
         }
-        public void setNum(long num) {
-            this.num = num;
+
+        public void setP2(int p2) {
+            this.p2 = p2;
         }
+
+        public BigDecimal getP3() {
+            return p3;
+        }
+
+        public void setP3(BigDecimal p3) {
+            this.p3 = p3;
+        }
+
+        public List<String> getP4() {
+            return p4;
+        }
+
+        public void setP4(List<String> p4) {
+            this.p4 = p4;
+        }
+
+        public Map<String, Object> getP5() {
+            return p5;
+        }
+
+        public void setP5(Map<String, Object> p5) {
+            this.p5 = p5;
+        }
+
         @Override
         public String toString() {
             return "To{" +
-                    "name='" + name + '\'' +
-                    ", num=" + num +
+                    "p1='" + p1 + '\'' +
+                    ", p2=" + p2 +
+                    ", p3=" + p3 +
+                    ", p4=" + p4 +
+                    ", p5=" + p5 +
                     '}';
         }
     }
