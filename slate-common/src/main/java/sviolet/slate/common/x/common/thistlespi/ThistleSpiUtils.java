de509d3a465b570716d940a0c9ae72ef2ba5fa42
shepherdviolet <shepherdviolet@163.com>
2018-10-17 21:03:20 +0800

    调整日志

--- slate-common/src/main/java/sviolet/slate/common/x/common/thistlespi/ThistleSpiUtils.java
+++ slate-common/src/main/java/sviolet/slate/common/x/common/thistlespi/ThistleSpiUtils.java
@@ -23,21 +23,51 @@ public class ThistleSpiUtils {
      * @return 强化版
      */
     public static SysPropFirstProperties wrapPropertiesBySysProp(Properties properties){
-        return new SysPropFirstProperties(properties, SYS_PROP_FIRST_PROPERTIES_EXCEPTION_HANDLER);
+        return new SysPropFirstProperties(properties, SYS_PROP_FIRST_PROPERTIES_LOG_HANDLER);
     }
 
-    private static SysPropFirstProperties.ExceptionHandler SYS_PROP_FIRST_PROPERTIES_EXCEPTION_HANDLER = new SysPropFirstProperties.ExceptionHandler() {
+    private static SysPropFirstProperties.LogHandler SYS_PROP_FIRST_PROPERTIES_LOG_HANDLER = new SysPropFirstProperties.LogHandler() {
+
         /**
-         * 当value从String转为指定类型时发生异常.
-         * 你可以在这里打印日志, get方法会返回默认值, 或抛出RuntimeException, get方法就会抛出该异常.
+         * 当系统参数(启动参数)存在, 并覆盖了内置参数时
+         * @param sysPropKey 系统参数key
+         * @param sysPropValue 系统参数的值
+         * @param propKey 内置参数的key
+         * @param propValue 内置参数的值
+         * @param defValue 预设的默认值
+         * @param properties 内置的Properties对象(不是系统参数)
+         */
+        @Override
+        public void onOverwrittenBySysProp(String sysPropKey, String sysPropValue, String propKey, String propValue, Object defValue, Properties properties) {
+            logger.info("?" + ThistleSpi.LOG_PREFIX_LOADER + "Constructor parameter '" + propKey + "' is overwritten by system property -D" + sysPropKey + "=" + sysPropValue + ", " + propValue + " -> " + sysPropValue);
+        }
+
+        /**
+         * 当使用默认值时
+         * @param sysPropKey 系统参数key
+         * @param sysPropValue 系统参数的值
+         * @param propKey 内置参数的key
+         * @param propValue 内置参数的值
+         * @param defValue 预设的默认值
+         * @param properties 内置的Properties对象(不是系统参数)
+         */
+        @Override
+        public void onUsingDefault(String sysPropKey, String sysPropValue, String propKey, String propValue, Object defValue, Properties properties) {
+            logger.info("?" + ThistleSpi.LOG_PREFIX_LOADER + "Constructor parameter '" + propKey + "' using default value '" + defValue + "'");
+        }
+
+        /**
+         * 当value从String转为指定类型时发生异常. 你可以在这里打印日志(get方法会返回默认值)
          * @param parsingSysProp true:解析系统参数(启动参数)发生错误 false:解析内置的Properties参数发生错误
          * @param key 发生错误的key
          * @param value 发生错误的value
+         * @param toType 尝试将value转换为该类型时出错
          * @param defValue 尝试使用的默认值
-         * @param properties 内置的Properties(不是系统Properties)
+         * @param properties 内置的Properties对象(不是系统参数)
+         * @param e 异常
          */
         @Override
-        public void onParseException(boolean parsingSysProp, String key, String value, Class<?> toType, String defValue, Properties properties, Exception e) {
+        public void onParseException(boolean parsingSysProp, String key, String value, Class<?> toType, Object defValue, Properties properties, Exception e) {
             if (parsingSysProp) {
                 logger.warn("?" + ThistleSpi.LOG_PREFIX_LOADER + "WARNING: Error while parsing system property '-D" + key + "=" + value + "' to " + toType.getName() + ", try using '" + defValue + "'", e);
             } else {
