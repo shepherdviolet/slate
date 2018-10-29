956761aae5e304e36843136962b8877e7c524363
shepherdviolet <shepherdviolet@163.com>
2018-10-29 20:24:05 +0800

    动态添加报错时只打印日志, 不抛出异常

--- slate-springboot/src/main/java/sviolet/slate/springboot/x/net/loadbalance/auto/HttpClientsImpl.java
+++ slate-springboot/src/main/java/sviolet/slate/springboot/x/net/loadbalance/auto/HttpClientsImpl.java
@@ -166,10 +166,15 @@ class HttpClientsImpl implements HttpClients, Closeable, InitializingBean, Dispo
 
                     //Check if new
                     if (client == null) {
-                        client = HttpClientCreator.create(tag, new HttpClientProperties());
-                        client.start();
-                        clients.put(tag, client);
-                        logger.info("HttpClients SettingsOverride | " + tag + "> Create new HttpClient with default properties, because no HttpClient named " + tag + " before");
+                        try {
+                            client = HttpClientCreator.create(tag, new HttpClientProperties());
+                            client.start();
+                            clients.put(tag, client);
+                            logger.info("HttpClients SettingsOverride | " + tag + "> Create new HttpClient with default properties, because no HttpClient named " + tag + " before");
+                        } catch (Exception e) {
+                            logger.debug("HttpClients SettingsOverride | Error while creating new HttpClient named " + tag + ", skip key " + key, e);
+                            continue;
+                        }
                     }
 
                     //Check if changed
